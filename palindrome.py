5f19534cc88b323d5ea6cdf02af041a45d4d64b9
nikita gandhare <nikitagandhare610@gmail.com>
2025-01-22 17:59:38 +0530

    add palindrome file

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+def palindrome(string):
+    return string
+string=input("enter a word of phrase")
+if (string==string[::-1]):
+    print(f" {string} is palindrome")
+else:
+    print(f"{string} is not palindrome")
+
+
+
+
+
+
+
+           
